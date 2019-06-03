5fceb37911214a6260daaf7ee05384f19776edbd
savannah8 <willengitonga.wg@gmail.com>
2019-06-03 13:02:03 +0300

    generated user model

--- app/email.py
+++ app/email.py
@@ -1,7 +1,7 @@
 from flask_mail import Message
 from flask import render_template
 from . import mail
-subject_pref = 'Watchlist'
+subject_pref = 'The Blog'
 sender_email ='willengitonga.wg@gmail.com'
 
 
